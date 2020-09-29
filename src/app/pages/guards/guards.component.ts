c4f39487ac31b03679ca325bc8a031adb5b916c5
Javier Muñoz <x_jemd_x@hotmail.com>
2020-09-28 22:18:47 -0300

    http get guards to api structure

--- src/app/pages/guards/guards.component.ts
+++ src/app/pages/guards/guards.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
+import { Guard } from '../../models/guard.interface'
 
 @Component({
   selector: 'app-guards',
@@ -7,17 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuardsComponent implements OnInit {
 
-  guards = [
-    {id: 1, name: "guard 1", description: "guard 1", email: "guard@email.com"},
-    {id: 2, name: "guard 2", description: "guard 2", email: "guard2@email.com"},
-    {id: 3, name: "guard 3", description: "guard 3", email: "guard3@email.com"},
-    {id: 4, name: "guard 4", description: "guard 4", email: "guard4@email.com"}
+  guards: Guard = [
+    {id: 1, name: "guard 1", email: "guard@email.com"},
+    {id: 2, name: "guard 2", email: "guard2@email.com"},
+    {id: 3, name: "guard 3", email: "guard3@email.com"},
+    {id: 4, name: "guard 4", email: "guard4@email.com"}
   ];
   selectedEmployee;
 
-  constructor() { }
+  constructor(
+    private api: ApiService
+  ) { }
 
   ngOnInit(): void {
+    this.api.getGuards().toPromise()
+      .then((data: any) => {
+        if(data.success === true){
+          this.guards = data.guards
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   public selectEmployee(e){
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment'
 import { Observable } from 'rxjs';
 import { User } from "../models/user.interface";
+import { Guard } from "../models/guard.interface";
+import { Client } from "../models/client.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +19,14 @@ export class ApiService {
 
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>(this.url + '/auth/login', { email, password })
+  }
+
+  getGuards(): Observable<Guard> {
+    return this.http.get<Guard>(this.url + '/auth/guards')
+  }
+
+  getClients(): Observable<Client> {
+    return this.http.get<Client>(this.url + '/auth/clients')
   }
 
 }
