af540b62b69a69bf176c576d06a2012836c47e7b
Eduardo C <blezthor666@gmail.com>
2021-01-21 17:30:11 -0300

    rut format

--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -25,7 +25,6 @@ export class ClientsComponent implements OnInit {
 
   ngOnInit(): void {
     this.clients$ = this.api.getClients()
-    console.log(this.today)
   }
 
   openModal(client) {
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -11,7 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
   
   loginForm: FormGroup;
-
+  variableRut
   constructor(
     public formBuilder: FormBuilder,
     public ngZone: NgZone, // NgZone service to remove outside scope warning
@@ -34,5 +34,45 @@ export class LoginComponent implements OnInit {
     let rut = this.loginForm.value.rut;
     let password = this.loginForm.value.password;
     this.auth.login(rut, password)
+  }
+
+  rut(rut: any) { // format rut8 and 9 digit
+    let div1, div2, div3, div4, formatRut;
+    let data = rut.target.value
+
+    switch (data.length) {
+      case 1:
+        return data;
+      case 2:
+        return data;
+      case 3:
+        return data;
+      case 4:
+        return data;
+      case 5:
+        return data;
+      case 6:
+        return data;
+      case 7:
+        return data;
+      case 8:
+        div1 = data.slice(0, 1);
+        div2 = data.slice(1, 4);
+        div3 = data.slice(4, 7);
+        div4 = data.slice(7, 8);
+        formatRut = div1 + '.' + div2 + '.' + div3 + '-' + div4
+        this.variableRut = formatRut
+        break;
+      case 9:
+        div1 = data.slice(0, 2);
+        div2 = data.slice(2, 5);
+        div3 = data.slice(5, 8);
+        div4 = data.slice(8, 9);
+        formatRut = div1 + '.' + div2 + '.' + div3 + '-' + div4
+        this.variableRut = formatRut
+        break;
+      }
+      return this.variableRut
+
   }
 }
